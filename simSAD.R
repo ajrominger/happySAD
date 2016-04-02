2d2d237cfa6e5acc71f6b6b497c8e9b7188a8c60
ajrominger <ajrominger@gmail.com>
2016-04-02 12:10:30 -0700

    simulation script

---
+++ simSAD.R
@@ -0,0 +1,11 @@
+## ======================================================================
+## simulation that generates random SAD data under different conditions:
+##   1) different numbers of species
+##   2) different subsampling sizes
+##   3) different models
+##   4) different parameter values
+## and then tests:
+##   1) AIC methods
+##   2) likelihood goodness of fit methods
+##   3) mse goodness of fit methods on both rank and cumulative functions
+## ======================================================================
