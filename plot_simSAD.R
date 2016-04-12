58443a080bf3edc839e5dbcf1ceb299fb9a33b0f
ajrominger <ajrominger@gmail.com>
2016-04-12 14:13:37 -0700

    finalized plots for theoretical SADs

--- plot_simSAD.R
+++ plot_simSAD.R
@@ -20,45 +20,38 @@ names(cols) <- c('fish', 'plnorm', 'stick', 'tnegb')
 ## plot theoretical SADs
 ## =====================
 
-par(mfrow=c(5, 4), mar=rep(0.5, 4), oma=c(4, 3, 1, 3)+0.1)
+sad.rad <- lapply(names(sad.par), function(m) {
+    lapply(1:4, function(i) {
+        s <- sad(model=m, par=sad.par[[m]][[i]])
+        return(sad2Rank(s, S=nspp[4]))
+    })
+})
+names(sad.rad) <- names(sad.par)
 
-for(i in 1:length(sad.par)) {
-    if(names(sad.par[i]) %in% c('plnorm', 'tnegb')) {
-        dfun <- function(x, par) get(sprintf('d%s', names(sad.par[i])))(x, par[[1]][1], par[[1]][2])
-        rfun <- function(n, par) get(sprintf('r%s', names(sad.par[i])))(n, par[[1]][1], par[[1]][2])
-    } else {
-        dfun <- function(x, par) get(sprintf('d%s', names(sad.par[i])))(x, par)
-        rfun <- function(n, par) get(sprintf('r%s', names(sad.par[i])))(n, par)
-    }
-    
-    ylim <- range(sapply(sad.par[[i]], function(p) {
-        if(length(p) > 1) p <- list(p)
-        dfun(c(1, 50), p)
-    }))
-    
-    
+pdf(file='ms/fig/fig_sadsUsed.pdf', width=4, height=4)
+par(mfcol=c(4, 4), mar=rep(0.5, 4), oma=c(2, 4, 2, 0)+0.1, mgp=c(2, 0.75, 0))
+
+ylim <- range(unlist(sad.rad))
+
+for(i in 1:4) {
+    mod <- names(sad.rad)[i]
     for(j in 1:4) {
-        plot(dfun(1:50, sad.par[[i]][j]), log='xy', type='l', xaxt='n', yaxt='s', lwd=3, ylim=ylim)
-        if(i == 5) logAxis(1)
-        
-        legend('topright', legend=mean(rfun(100, sad.par[[i]][j])))
+        plot(sad.rad[[i]][[j]], ylim=ylim, type='l', log='y', xaxt='n', yaxt='n',
+             col=cols[mod], lwd=2)
+        if(i == 1) logAxis(2)
+        if(j == 1) mtext(switch(mod, 'fish'='Logseries',
+                                'plnorm'='Pois LogNorm',
+                                'stick'='Broken Stick',
+                                'tnegb'='Trunc NegBin'), 
+                         side=3, line=1, cex=0.8, col=cols[mod])
     }
-    
-    mtext(names(sad.par[i]), side=4, line=1, xpd=NA)
 }
 
-mtext('Abundance', side=1, line=3, outer=TRUE)
-mtext('Probability', side=2, line=2, outer=TRUE)
+mtext('Species rank', side=1, line=1, outer=TRUE)
+mtext('Abundance', side=2, line=2, outer=TRUE)
 
+dev.off()
 
-par(mfrow=c(length(sad.par), 4), mar=c(1.5, 1.5, 0, 0)+0.1, mgp=c(2, 0.4, 0), tcl=-0.4, oma=c(4, 4, 0, 0))
-for(i in 1:length(sad.par)) {
-    for(j in 1:4) {
-        r <- sort(sample.sad(sad.rfun[[i]][[j]](nspp[4]), prob=prop[4]), TRUE)
-        plot(r, xlab='', ylab='', log='y', type='l', ylim=c(1, max(r)))
-        for(k in 1:3) lines(sort(sample.sad(sad.rfun[[i]][[j]](nspp[4]), prob=prop[k]), TRUE))
-    }
-}
 
 
 ## ========================
@@ -124,6 +117,7 @@ aic.sum <- aggregate(aic.sum[, c('fishWin', 'plnormWin', 'stickWin', 'tnegbWin')
 
 
 ## plot AIC
+pdf('ms/fig/fig_aic.pdf', width=4, height=4)
 par(mfcol=c(4, 4), mar=rep(0.5, 4), oma=c(3, 3, 2, 2)+0.1, mgp=c(1, 0.1, 0))
 
 for(mod in as.character(unique(aic.sum$actualDist))) {
@@ -147,5 +141,6 @@ for(mod in as.character(unique(aic.sum$actualDist))) {
 }
 
 mtext('Different parameterizations', side=1, line=1.5, outer=TRUE)
-mtext('Proportion model support', side=2, line=1, outer=TRUE)
+mtext('Relative model support', side=2, line=1, outer=TRUE)
 
+dev.off()
