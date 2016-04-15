89fbcce9a12bb250b3e2014457c4fa1162650355
ajrominger <ajrominger@gmail.com>
2016-04-15 16:58:04 -0700

    testing gambin model

---
+++ test_gambin.R
@@ -0,0 +1,66 @@
+setwd('~/Dropbox/Research/happySAD')
+devtools::load_all('../pika')
+library(gambin)
+
+## funciton to calculate probabilities for binned fisher log series from raw data
+dBinFish <- function(x, beta, log=FALSE) {
+    maxBinX <- (2^(1+floor(log(max(x), 2)))-1)
+    probs <- dfish(1:maxBinX, beta)
+    binProbs <- tapply(probs, floor(log(1:maxBinX, 2)), sum)
+    binProbs <- binProbs/sum(binProbs)
+    newx <- create_octaves(x)[, 2]
+    
+    if(log) {
+        return(log(binProbs) * newx)
+    } else {
+        return(exp(newx * log(binProbs)))
+    }
+}
+
+## function to 'unbin' data
+unbin <- function(bins, freq) {
+    rep(2^bins, round(freq))
+}
+
+## fit the binned fisher log series
+fitBinFish <- function(x) {
+    fun <- function(b) -sum(dBinFish(x, b, log=TRUE))
+    out <- optimize(fun, interval=c(.Machine$double.eps, 30))
+    out <- c(out[[1]], out[[2]])
+    names(out) <- c('MLE', 'll')
+    out[2] <- -out[2]
+    
+    return(out)
+}
+
+
+## set-up simulation
+bs <- 10^(-4:0)
+lapply(bs, function(b) {
+    ## simulate from fisher
+    r <- rfish(100, 0.1)
+    
+    ## fit gambin and extract info
+    gbfit <- fitGambin(r)
+    
+    ## fit fisher to raw
+    sum(dBinFish(r, 0.1, log=TRUE))
+    
+    ## fit binned fisher
+})
+
+fitBinFish(r)
+sad(r, 'fish')
+
+
+
+
+bla <- gambin_exp(7.1, 11, 100)
+ubla <- unbin((1:length(bla)) - 1, bla)
+ublaf <- sad(ubla, 'fish')
+
+logLik(fitGambin(ubla))
+dgambin
+
+plot(dBinFish(2^((1:length(bla)) - 1), ublaf$MLE))
+points(dgambin(7.1, 11))
