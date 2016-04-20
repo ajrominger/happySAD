d3dc311117a60c97cd2033ed42057d81c98c5a21
ajrominger <ajrominger@gmail.com>
2016-04-20 14:39:52 -0700

    summarizing all likelihoods now, changed from previously not including `fish'

--- test_gambin_plotting.R
+++ test_gambin_plotting.R
@@ -6,9 +6,9 @@ load('gambin_sim.RData')
 
 gb.sim <- cbind(model=rownames(gb.sim), as.data.frame(gb.sim))
 
-llComp <- with(gb.sim[gb.sim$model != 'fish', ], aggregate(list(ll=ll), 
-                                                           list(truPar=truPar, model=model), 
-                                                           function(x) c(mean=mean(x), sd=sd(x))))
+llComp <- with(gb.sim, aggregate(list(ll=ll), 
+                                 list(truPar=truPar, model=model), 
+                                 function(x) c(mean=mean(x), sd=sd(x))))
 
 plot(llComp$truPar, llComp$ll[, 'mean'], log='x')
 
