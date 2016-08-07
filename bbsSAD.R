6d93d978f28b8f148f6b641f3da7e16056f7ac3c
ajrominger <ajrominger@gmail.com>
2016-08-07 20:21:47 +0200

    added example sad figure

--- bbsSAD.R
+++ bbsSAD.R
@@ -1,6 +1,8 @@
-## devtools::load_all('../pika') 
+## loading pika without dependencies
+# devtools::load_all('../pika') 
 setwd('~/Dropbox/Research/pika/R')
 sapply(list.files(), source)
+devtools::load_all('~/Dropbox/Research/socorro') 
 
 setwd('~/Dropbox/Research/happySAD')
 
@@ -8,6 +10,14 @@ setwd('~/Dropbox/Research/happySAD')
 bbsYear <- 2009
 bbs <- read.csv(sprintf('~/Research/datasets/bbs/db/bbs%s.csv', bbsYear), as.is = TRUE)
 head(bbs)
+
+## make a quick plot for ESA presentation
+this.sad <- sad(bbs$abund[bbs$route==unique(bbs$route)[2]], 'tnegb', keepData = TRUE)
+
+
+plot(sort(this.sad$data, TRUE), log='y')
+lines(sad2Rank(this.sad))
+
 
 x <- lapply(unique(bbs$route)[1:2], function(r) {
     fit <- fitSAD(bbs$abund[bbs$route == r], models = c('tnegb', 'fish'), keepData = TRUE)
