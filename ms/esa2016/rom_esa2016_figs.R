1b87166744d46210e49aeaac3fdc9f1999455008
ajrominger <ajrominger@gmail.com>
2016-08-09 20:32:45 -0400

    finalized z val map

--- ms/esa2016/rom_esa2016_figs.R
+++ ms/esa2016/rom_esa2016_figs.R
@@ -36,23 +36,29 @@ colVal <- function(x, xfun, cols = hsv(c(0.55, 0.75), c(0.3, 0.8), c(1, 0.5))) {
     
     newx <- xfun(x)
     m <- 1 / diff(range(newx))
-    out <- 
     
     function(v) rgb(cfun(m * (xfun(v) - min(newx))), maxColorValue = 255)
 }
 
+
+## negb z values 
+
+pdf('fig_bbsNegbZ.pdf', width=5, height=5)
+
 with(bbsSAD[!is.na(bbsSAD$z), ], {
-    browser()
     zfun <- colVal(z, function(x) -log(x, 10))
     zrange <- range(-log(z, 10))
     
     d <- density(-log(z, 10), from = zrange[1], to = zrange[2])
     
     layout(matrix(1:2, ncol = 1), heights = c(1, 3))
-    par(mar=c(4, 2, 0, 2) + 0.1, mgp = c(1.5, 0.75, 0))
+    par(mar=c(1, 2, 0, 2) + 0.1, mgp = c(1.75, 0.75, 0))
     
-    plot(d, axes = FALSE, ylim = c(min(d$y) - 0.3*diff(range(d$y)), max(d$y)), 
-         main = '', xlab = expression(z^2*'-value'), ylab = '')
+    plot(d, axes = FALSE, 
+         xlim = rev(zrange),
+         ylim = c(min(d$y) - 0.3*diff(range(d$y)), max(d$y)), 
+         main = '', xlab = '', ylab = '')
+    mtext(expression(z^2*'-value'), side=1, line=2)
     
     rect(xleft = seq(zrange[1], zrange[2] - 0.01*diff(zrange), by = 0.01*diff(zrange)), 
          xright = seq(zrange[1] + 0.01*diff(zrange), zrange[2], by = 0.01*diff(zrange)),
@@ -63,24 +69,56 @@ with(bbsSAD[!is.na(bbsSAD$z), ], {
     axis(1, at = seq(0, 10, by = 2), labels = 10^-seq(0, 10, by = 2))
     
     par(mar=c(0.1, 2.1, 0, 2.1))
+    
     map('world', c('usa', 'canada'), xlim = c(-180, -50), col=NA)
     map('world', c('canada', 'alaska'), add=TRUE, col='gray', fill=TRUE, border='gray')
     map('world', 'USA:alaska', add=TRUE, col='gray', fill=TRUE, border='gray')
     map('usa', add=TRUE, col='gray', fill=TRUE, border='gray')
 
-    points(Longi, Lati, col=zfun(z), pch=16, cex=0.5)
-    
-    
+    points(Longi, Lati, col=zfun(z), pch=16, cex=0.4)
 })
 
+dev.off()
 
-with(bbsSAD[!is.na(bbsSAD$z), ], {
-    bla <- colVal(z, function(x) -log(x))
-    plot(sort(z)[1:10], col=bla(sort(z)[1:10]), pch=16, cex=0.5, log='y')
+
+## map of AIC winner
+
+pdf('fig_bbsAIC.pdf', width=5, height=5)
+
+with(bbsSAD[bbsSAD$dAIC == 0, ], {
+    zfun <- colVal(z, function(x) -log(x, 10))
+    zrange <- range(-log(z, 10))
+    
+    d <- density(-log(z, 10), from = zrange[1], to = zrange[2])
+    
+    layout(matrix(1:2, ncol = 1), heights = c(1, 3))
+    par(mar=c(1, 2, 0, 2) + 0.1, mgp = c(1.75, 0.75, 0))
+    
+    plot(d, axes = FALSE, 
+         xlim = rev(zrange),
+         ylim = c(min(d$y) - 0.3*diff(range(d$y)), max(d$y)), 
+         main = '', xlab = '', ylab = '')
+    mtext(expression(z^2*'-value'), side=1, line=2)
+    
+    rect(xleft = seq(zrange[1], zrange[2] - 0.01*diff(zrange), by = 0.01*diff(zrange)), 
+         xright = seq(zrange[1] + 0.01*diff(zrange), zrange[2], by = 0.01*diff(zrange)),
+         ybottom = par('usr')[3], ytop = min(d$y) - 0.01*diff(range(d$y)), 
+         col = zfun(10^-seq(zrange[1], zrange[2] - 0.01*diff(zrange), by = 0.01*diff(zrange))),
+         border = NA)
+    
+    axis(1, at = seq(0, 10, by = 2), labels = 10^-seq(0, 10, by = 2))
+    
+    par(mar=c(0.1, 2.1, 0, 2.1))
+    
+    map('world', c('usa', 'canada'), xlim = c(-180, -50), col=NA)
+    map('world', c('canada', 'alaska'), add=TRUE, col='gray', fill=TRUE, border='gray')
+    map('world', 'USA:alaska', add=TRUE, col='gray', fill=TRUE, border='gray')
+    map('usa', add=TRUE, col='gray', fill=TRUE, border='gray')
+
+    points(Longi, Lati, col=zfun(z), pch=16, cex=0.4)
 })
 
-points(bbsSAD$Longi, bbsSAD$Lati)
-
+dev.off()
 
 ## lognorm and gamma examples
 pdf('fig_lnorm.pdf', width = 3.25, height = 3.25)
