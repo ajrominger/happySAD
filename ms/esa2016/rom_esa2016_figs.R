19942ad80e3fc8e5c5c3ed9daded6794b2f1909c
ajrominger <ajrominger@gmail.com>
2016-08-10 09:44:59 -0400

    made negb k figure

--- ms/esa2016/rom_esa2016_figs.R
+++ ms/esa2016/rom_esa2016_figs.R
@@ -3,6 +3,7 @@ devtools::load_all('../../../../pika')
 devtools::load_all('../../../../socorro')
 library(maps)
 library(mapdata)
+library(plyr)
 
 ## BBS example
 bbsYear <- 2009
@@ -82,31 +83,36 @@ dev.off()
 
 
 ## map of AIC winner
+bbsAICWin <- ddply(bbsSAD, 'route', function(x) {
+    if(x$dAIC[x$mod == 'tnegb'] <= 2) m <- 'tnegb'
+    else m <- x$mod[x$dAIC == 0]
+    
+    return(data.frame(Longi=x$Longi[1], Lati=x$Lati[1], mod=m))
+})
 
 pdf('fig_bbsAIC.pdf', width=5, height=5)
 
-with(bbsSAD[bbsSAD$dAIC == 0, ], {
-    zfun <- colVal(z, function(x) -log(x, 10))
-    zrange <- range(-log(z, 10))
-    
-    d <- density(-log(z, 10), from = zrange[1], to = zrange[2])
+modCol <- hsv(c(0.52, 0.02, 0.12, 0.65), c(0.8, 0.7, 0.8, 0.7), c(0.8, 0.75, 0.9, 0.6))
+names(modCol) <- c('fish', 'plnorm', 'stick', 'tnegb')
+
+with(bbsAICWin, {
+    win <- sort(table(mod))
     
     layout(matrix(1:2, ncol = 1), heights = c(1, 3))
-    par(mar=c(1, 2, 0, 2) + 0.1, mgp = c(1.75, 0.75, 0))
+    par(mar=c(0, 2, 4, 2) + 0.1, mgp = c(1.75, 0.75, 0))
     
-    plot(d, axes = FALSE, 
-         xlim = rev(zrange),
-         ylim = c(min(d$y) - 0.3*diff(range(d$y)), max(d$y)), 
-         main = '', xlab = '', ylab = '')
-    mtext(expression(z^2*'-value'), side=1, line=2)
+    plot(1, xlim = c(0, sum(win)), type = 'n', xaxs = 'i', axes = FALSE, xlab = '', ylab = '')
     
-    rect(xleft = seq(zrange[1], zrange[2] - 0.01*diff(zrange), by = 0.01*diff(zrange)), 
-         xright = seq(zrange[1] + 0.01*diff(zrange), zrange[2], by = 0.01*diff(zrange)),
-         ybottom = par('usr')[3], ytop = min(d$y) - 0.01*diff(range(d$y)), 
-         col = zfun(10^-seq(zrange[1], zrange[2] - 0.01*diff(zrange), by = 0.01*diff(zrange))),
+    rect(xleft = c(0, cumsum(win)[-length(win)]), xright = c(cumsum(win)),
+         ybottom = par('usr')[3], ytop = par('usr')[4], 
+         col = modCol[names(win)],
          border = NA)
+    text(cumsum(win) - win/2, mean(par('usr')[3:4]), labels = win, col = 'white', cex = 1.2)
     
-    axis(1, at = seq(0, 10, by = 2), labels = 10^-seq(0, 10, by = 2))
+    box()
+    
+    mtext(c('Pois\nlognorm', 'log-\nseries', 'neg binom'), line = 0.5, at = cumsum(win) - win/2,
+          col = modCol[names(win)])
     
     par(mar=c(0.1, 2.1, 0, 2.1))
     
@@ -115,10 +121,58 @@ with(bbsSAD[bbsSAD$dAIC == 0, ], {
     map('world', 'USA:alaska', add=TRUE, col='gray', fill=TRUE, border='gray')
     map('usa', add=TRUE, col='gray', fill=TRUE, border='gray')
 
-    points(Longi, Lati, col=zfun(z), pch=16, cex=0.4)
+    points(Longi, Lati, col=modCol[as.character(mod)], pch=16, cex=0.4)
 })
 
 dev.off()
+
+
+## negb k values 
+
+bbsNegB <- ddply(bbs, 'route', function(x) sad(x$abund, model = 'tnegb')$MLE)
+bbsNegB <- cbind(bbsNegB, bbsInfo[match(bbsNegB$route, bbsInfo$route.id), c('Longi', 'Lati')])
+names(bbsNegB) <- c('route', 'mu', 'k', 'Longi', 'Lati')
+rownames(bbsNegB) <- NULL
+
+
+pdf('fig_bbsNegbK.pdf', width=5, height=5)
+
+with(bbsNegB, {
+    zfun <- colVal(k, function(x) x, cols = hsv(c(0.15, 0.03), c(0.55, 0.8), c(0.95, 0.5)))
+    zrange <- range(k)
+    
+    d <- density(k, from = zrange[1], to = zrange[2])
+    
+    layout(matrix(1:2, ncol = 1), heights = c(1, 3))
+    par(mar=c(1, 2, 0, 2) + 0.1, mgp = c(1.75, 0.75, 0))
+    
+    plot(1, axes = FALSE, type = 'n',
+         xlim = zrange, 
+         ylim = c(min(d$y) - 0.3*diff(range(d$y)), max(d$y)), 
+         main = '', xlab = '', ylab = '')
+    mtext('Neg binom k parameter', side=1, line=2)
+    axis(1)
+    
+    rect(xleft = seq(zrange[1], zrange[2] - 0.01*diff(zrange), by = 0.01*diff(zrange)), 
+         xright = seq(zrange[1] + 0.01*diff(zrange), zrange[2], by = 0.01*diff(zrange)),
+         ybottom = par('usr')[3], ytop = min(d$y) - 0.01*diff(range(d$y)), 
+         col = zfun(seq(zrange[1], zrange[2] - 0.01*diff(zrange), by = 0.01*diff(zrange))),
+         border = NA)
+    rect(xleft = zrange[1], xright = zrange[2],
+         ybottom = par('usr')[3], ytop = min(d$y) - 0.01*diff(range(d$y)))
+    
+    par(mar=c(0.1, 2.1, 0, 2.1))
+    
+    map('world', c('usa', 'canada'), xlim = c(-180, -50), col=NA)
+    map('world', c('canada', 'alaska'), add=TRUE, col='gray', fill=TRUE, border='gray')
+    map('world', 'USA:alaska', add=TRUE, col='gray', fill=TRUE, border='gray')
+    map('usa', add=TRUE, col='gray', fill=TRUE, border='gray')
+
+    points(Longi, Lati, col=zfun(k), pch=16, cex=0.4)
+})
+
+dev.off()
+
 
 ## lognorm and gamma examples
 pdf('fig_lnorm.pdf', width = 3.25, height = 3.25)
