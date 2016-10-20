a8b22e2c4e1ea78d018ffa8c3b269d0db5016a58
ajrominger <ajrominger@gmail.com>
2016-10-19 17:47:34 -0700

    testing out mse too

--- sim_zValAcrossN.R
+++ sim_zValAcrossN.R
@@ -1,31 +1,39 @@
 library(pika)
 library(parallel)
 
-N <- seq(10, 100, by = 15)
+N <- round(exp(seq(log(10), log(200), length = 10)) / 5) * 5
 
 zn <- lapply(N, function(n) {
     out <- mclapply(1:100, mc.cores = 6, FUN = function(i) {
         print(paste(n, ': ', i, sep = ''))
-        s <- sad(x = rtnegb(n, 10, 10), model = 'tpois')
-        logLikZ(s, nrep = 500)$z
+        s <- sad(x = rtnegb(n, 10, 20), model = 'tpois', keepData = TRUE)
+        z <- logLikZ(s, nrep = 500)$z
+        e <- mseZ(s, nrep = 500, type = 'rank', relative = FALSE, log = FALSE)$z
+        return(c(z = z, e = e))
     })
-    out <- unlist(out)
+    out <- do.call(rbind, out)
     
-    return(mean = c(mean(out), quantile(out, prob = c(0.025, 0.975))))
+    return(c(z = c(mean = mean(out[, 1]), quantile(out[, 1], prob = c(0.025, 0.975))),
+             e = c(mean = mean(out[, 2]), quantile(out[, 2], prob = c(0.025, 0.975)))))
 })
 
 zn <- data.frame(n = N, do.call(rbind, zn))
+names(zn) <- c('n', 'z.mean', 'z.ciLo', 'z.ciHi', 'e.mean', 'e.ciLo', 'e.ciHi')
 
-x <- sad(rtnegb(100, 10, 10), model = 'tpois', keepData = TRUE)
-plot(x, ptype = 'rad')
+plot(zn$n, zn$e.mean)
 
-xr <- getrfun(x)
-foo <- replicate(500, sad(xr(100), 'stick')$ll)
 
-plot(density(foo), xlim = range(foo, x$ll))
-abline(v = x$ll)
+## now try with meteR's state var approach
 
-foo <- logLikZ(x, nrep = 999, return.sim = TRUE)
-foo$z
-
+zmeteRn <- lapply(N, function(n) {
+    out <- mclapply(1:100, mc.cores = 6, FUN = function(i) {
+        print(paste(n, ': ', i, sep = ''))
+        s <- meteR::sad(meteR::meteESF(spp = as.character(1:n), abund = rtnegb(n, 8, 1)))
+        z <- meteR::logLikZ(s, nrep = 500)$z
+        return(z)
+    })
+    out <- unlist(out)
+    browser()
+    return(c(mean = mean(out), quantile(out, prob = c(0.025, 0.975))))
+})
 
