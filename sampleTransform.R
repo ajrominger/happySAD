f30bd2b28b1005678a74aa56766cfe8e5accece3
ajrominger <ajrominger@gmail.com>
2017-06-15 11:50:34 -0600

    trying to understand the sample transform of dewdney

---
+++ sampleTransform.R
@@ -0,0 +1,24 @@
+x <- rfish(1000, 0.01)
+xsad <- sad(x, model = 'fish', keepData = TRUE)
+
+N <- round(exp(seq(log(1), log(0.75*max(x)), length.out = 50)))
+
+sampThr <- lapply(N, function(n) {
+    foo <- dhyper(n, 1:(10*max(x)), sum(x) - (1:(10*max(x))), round(0.5*sum(x)))
+    thr <- sum((foo * dfish(1:(10*max(x)), xsad$MLE))[is.finite(foo)])
+    
+    obs <- mean(replicate(100, {
+        s <- sample.sad(xsad, round(0.5*sum(x)))
+        mean(s == n)
+    }))
+    
+    return(c(thr = thr, obs = obs))
+})
+sampThr <- do.call(rbind, sampThr)
+
+plot(N[1:47], sampThr[1:47, 1], ylim = range(sampThr[1:47, ]), log = 'xy', 
+     type = 'b', pch = 16, cex = 0.5)
+points(N[1:47], sampThr[1:47, 2], type = 'b', cex = 0.6, pch = 16, col = 'red')
+
+plot(sampThr)
+abline(0, 1)
