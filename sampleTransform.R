0d00eaab07c5abc1f1413c3bc25eb08689dd307d
ajrominger <ajrominger@gmail.com>
2017-06-15 16:46:04 -0600

    cleaned up plotting

--- sampleTransform.R
+++ sampleTransform.R
@@ -1,7 +1,8 @@
-x <- rfish(1000, 0.01)
+x <- rfish(1000, 0.001)
 xsad <- sad(x, model = 'fish', keepData = TRUE)
+plot(xsad, ptype = 'rad', log = 'y')
 
-N <- round(exp(seq(log(1), log(0.75*max(x)), length.out = 50)))
+N <- round(exp(seq(log(1), log(0.5*max(x)), length.out = 50)))
 
 sampThr <- lapply(N, function(n) {
     foo <- dhyper(n, 1:(10*max(x)), sum(x) - (1:(10*max(x))), round(0.5*sum(x)))
@@ -15,10 +16,12 @@ sampThr <- lapply(N, function(n) {
     return(c(thr = thr, obs = obs))
 })
 sampThr <- do.call(rbind, sampThr)
+N <- N[sampThr[, 2] > 0]
+sampThr <- sampThr[sampThr[, 2] > 0, ]
 
-plot(N[1:47], sampThr[1:47, 1], ylim = range(sampThr[1:47, ]), log = 'xy', 
+plot(N, sampThr[, 1], ylim = range(sampThr), log = 'x',
      type = 'b', pch = 16, cex = 0.5)
-points(N[1:47], sampThr[1:47, 2], type = 'b', cex = 0.6, pch = 16, col = 'red')
+points(N, sampThr[, 2], type = 'b', cex = 0.6, pch = 16, col = 'red')
 
 plot(sampThr)
 abline(0, 1)
