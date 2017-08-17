3bbdecea8f80269c659148f3ee4b0b9ca5a519f6
ajrominger <ajrominger@gmail.com>
2017-08-17 16:59:52 -0600

    re-parameterized by scale, not rate; added SANN optim method with N-M fails

--- gamma-lnorm_klDist.R
+++ gamma-lnorm_klDist.R
@@ -2,24 +2,79 @@ library(plyr)
 library(parallel)
 library(viridis)
 
-dDklGamma <- function(x, shape, rate, meanlog, sdlog) {
-    dgamma(x, shape, rate) * (dgamma(x, shape, rate, log = TRUE) - dlnorm(x, meanlog, sdlog, log = TRUE))
+dDklGamma <- function(x, shape, scale, meanlog, sdlog) {
+    dgamma(x, shape, scale = scale) * (dgamma(x, shape, scale = scale, log = TRUE) - 
+                                           dlnorm(x, meanlog, sdlog, log = TRUE))
 }
 
-DklGamma <- function(shape, rate, meanlog, sdlog) {
-    integrate(function(x) dDklGamma(x, shape, rate, meanlog, sdlog), lower = 0, upper = Inf, rel.tol = .Machine$double.eps^0.8)
+DklGamma <- function(shape, scale, meanlog, sdlog) {
+    integrate(function(x) dDklGamma(x, shape, scale = scale, meanlog, sdlog), 
+              lower = 0, upper = Inf, 
+              rel.tol = .Machine$double.eps^0.25)
 }
 
-parRangeLong <- seq(0.01, 100, length.out = 20)
-parRangeShrt <- seq(0.01, 100, length.out = 6)
-pars <- expand.grid(shape = parRangeLong, rate = parRangeLong, meanlog = parRangeShrt, sdlog = parRangeShrt)
 
-gammaDkl <- unlist(mclapply(1:nrow(pars), mc.cores = 6, FUN = function(i) {
-    out <- try(DklGamma(pars[i, 1], pars[i, 2], pars[i, 3], pars[i, 4])$value, silent = TRUE)
-    if('try-error' %in% class(out)) out <- NA
-    return(out)
-}))
+parMin <- 0.1
+parMax <- 8
+badOptim <- 666666666
 
-image(matrix(gammaDkl[pars$meanlog == parRangeShrt[4] & pars$sdlog == parRangeShrt[4]], nrow = length(parRangeLong)), 
-      col = viridis(60))
-contour(matrix(gammaDkl[pars$meanlog == parRangeShrt[4] & pars$sdlog == parRangeShrt[4]], nrow = length(parRangeLong))^0.5)
+
+parRange <- seq(parMin, parMax, length.out = 5)
+par(mfcol = c(length(parRange), length(parRange)), mar = rep(1, 4))
+plot(1, type = 'n', axes = FALSE, xlab = '', ylab = '')
+for(i in 1:length(parRange)) {
+    for(j in 1:length(parRange)) {
+        par(mfg = c(length(parRange) - j + 1, i))
+        curve(dgamma(x, shape = parRange[i], scale = parRange[j]), from = 0.1, to = 100, 
+              ylim = c(0.00001, 0.2), log = 'x')
+    }
+}
+
+
+
+parRange <- seq(parMin, parMax, length.out = 20)
+pars <- expand.grid(shape = parRange, scale = parRange)
+gammaAsLnorm <- mclapply(1:nrow(pars), mc.cores = 6, function(i) {
+    out <- optim(par = c(1, 1), method = 'Nelder-Mead',
+                 fn = function(lnormPars) {
+                     x <- try(DklGamma(pars[i, 1], pars[i, 2], 
+                                       meanlog = lnormPars[1], sdlog = lnormPars[2])$value, 
+                              silent = TRUE)
+                     if('try-error' %in% class(x)) {
+                         return(badOptim)
+                     } else {
+                         return(x)
+                     }
+                 })
+    if(out$value == badOptim) {
+        out <- optim(par = c(1, 1), method = 'SANN',
+                     fn = function(lnormPars) {
+                         x <- try(DklGamma(pars[i, 1], pars[i, 2], 
+                                           meanlog = lnormPars[1], sdlog = lnormPars[2])$value, 
+                                  silent = TRUE)
+                         if('try-error' %in% class(x)) {
+                             return(badOptim)
+                         } else {
+                             return(x)
+                         }
+                     })
+    }
+    
+    return(unlist(out[1:2]))
+})
+
+gammaAsLnorm <- as.data.frame(do.call(rbind, gammaAsLnorm))
+gammaAsLnorm$value[gammaAsLnorm$value == badOptim] <- NA
+
+par(mfcol = c(1, 1))
+image(parRange, parRange, matrix(gammaAsLnorm$value, nrow = length(parRange)), col = viridis(60))
+range(gammaAsLnorm$value, na.rm = TRUE)
+
+
+mu <- seq(-13, 8, length.out = 20)
+sig <- seq(0.1, 24, length.out = 20)
+MuSig <- expand.grid(mu, sig)
+foo <- lapply(1:nrow(MuSig), function(i) {
+    DklGamma(shape = parRange[1], scale = parRange[5], meanlog = MuSig[i, 1], sdlog = MuSig[i, 2])
+})
+
