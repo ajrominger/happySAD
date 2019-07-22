893102c86118cdac198208ebc4cd89925f67b2dd
ajrominger <ajrominger@gmail.com>
2019-07-22 10:31:17 -0600

    added roxygen

--- R/dintModeGivenS.R
+++ R/dintModeGivenS.R
@@ -1,3 +1,26 @@
+#' @title Multinomial CDF
+#'
+#' @description Cumulative probability function of the multinomial distribution
+#'
+#' @details For Preston octaves 0 through 12, this function calculates the probability that each
+#' octave could be a modal octave (mode defined as equal to the maximum frequency of species).
+#' More than one octave could meet this criterian, so the returned vector of probabilities will
+#' not neccesarily sum to 1.  The probability of a mode at octave i is
+#' $\sum_s P(o_i = s) P(\text{all } o_{j \neq i} \leq s)$ where $P(o_i = s)$ is simply
+#' \code{dbinom(s, S, p[i])} where \code{S} is the total number of species and \code{p[i]} is the
+#' summed probability of the SAD corresponding to octave i. And where
+#' $P(\text{all } o_{j \neq i} \leq s)$ is given by \code{pmultinom(rep(s, 12), S - s, p[-i])}
+#'
+#' @param S number of spcies
+#' @param pars vector of parameters for the SAD model
+#' @param mod string specifying the SAD density function (e.g. \code{'dfish'})
+#'
+#' @return a vector of probabilities equal in length to number of octaves (13)
+#'
+#' @author Andy Rominger <ajrominger@@gmail.com>
+#'
+#' @export
+
 dintModeGivenS <- function(S, pars, mod) {
     noct <- 13
     x <- 1:noct
