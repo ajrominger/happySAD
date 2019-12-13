57c1f6b7691e9a36a71bfd696a24b91ffcf82c5f
ajrominger <ajrominger@gmail.com>
2019-12-13 08:45:28 -0700

    finalized dsingModeGivenS, make it use `.allLessMax`

--- R/dsingModeGivenS.R
+++ R/dsingModeGivenS.R
@@ -3,10 +3,17 @@
 #' @description For a given model and number of species, returns the probability of a
 #' single mode in the Preston plot
 #'
-#' @details A mode is defined by the cuttoff \code{modeCutOff} and so a mode must be greater
-#' than its neighbors by at least that value
+#' @details A mode is defined by the cuttoff \code{modeCutOff}. This can be made a proportional cuttoff if \code{relative = TRUE} is relative
+#' to the height of the peak. For example if \code{modeCutOff = 0.1, relative = TRUE} this sequence would contain a mode:
+#' \code{c(17, 20, 17)} but this one would not: \code{c(19, 20, 19)}.  The Probability of a single mode
+#' is numerically involved and so a Monte Carlo approximation is used with \code{B} replicates.
 #'
-#' @param x object to Prestonize
+#' @param S number of species
+#' @param mod the SAD model specified by the \code{d*} function, e.g. \code{dfish}
+#' @param pars model parameters
+#' @param modeCutOff the fraction by which a peak must exceed its neigbors to count as a mode
+#' @param relative logical, should \code{modeCutOff} be taken as relative or not
+#' @param B number of Monte Carlo iterations
 #'
 #' @return a numeric giving the desired probability
 #'
@@ -14,7 +21,7 @@
 #'
 #' @export
 
-dsingModeGivenS <- function(S, mod, pars, modeCutOff = 1, B = 1e+05) {
+dsingModeGivenS <- function(S, mod, pars, modeCutOff = 0.1, relative = TRUE, B = 1e+04) {
     noct <- 13
     x <- 1:noct
     nn <- 1:(2^(noct) - 1)
@@ -23,28 +30,30 @@ dsingModeGivenS <- function(S, mod, pars, modeCutOff = 1, B = 1e+05) {
     octProbs <- tapply(pp, octs, sum)
 
     r <- rmultinom(B, S, octProbs)
-    # plot(r, type = 'b')
-    # abline(h = seq(0, 80, by = modeCutOff))
 
-    # browser()
-    # o <- parallel::mclapply(1:ncol(r), mc.cores = 3, FUN = function(i) {
     o <- lapply(1:ncol(r), function(i) {
-        .allLessMax(r[, i], modeCutOff)
+        return(.allLessMax(r[, i], modeCutOff, relative))
+        return(.nmode(r[, i], modeCutOff) == 1)
     })
 
     return(mean(unlist(o)))
 }
 
-.allLessMax <- function(x, cutOff) {
-    imax <- which.max(x)
+.allLessMax <- function(x, cutOff, relative) {
+    imax <- which.max(rle(x)$values)
 
-    if(cutOff < 1) cutOff <- max(x) * cutOff
-    cat(cutOff, '\n')
+    if(relative) cutOff <- max(x) * cutOff
 
     maxsplit <- list(x[1:imax], x[imax:length(x)])
 
-    return(all(diff(maxsplit[[1]]) >= 1 - cutOff) &
-               all(diff(maxsplit[[2]]) <= -1 + cutOff))
+    return(all(diff(maxsplit[[1]]) > 1 - cutOff) &
+               all(diff(maxsplit[[2]]) < -1 + cutOff))
 }
 
-
+#
+# .nmode <- function(x, cutOff) {
+#     y <- log(x)
+#     y[y < 0] <- -1
+#
+#     length(localMaxima(y, by = log(1 / (1 - cutOff)), xmin = -1))
+# }
