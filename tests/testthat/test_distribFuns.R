edf16cd9bff623b04c8db02a9523c6487b4cc513
ajrominger <ajrominger@gmail.com>
2019-12-16 09:48:30 -0700

    uncommented tests

--- tests/testthat/test_distribFuns.R
+++ tests/testthat/test_distribFuns.R
@@ -3,42 +3,25 @@ context('distribution functions work')
 # ----
 # dsumuptpois
 
-# test_that('when upper truncation is large the `dsumuptpois` function is equivilant to un-truncated', {
-#     nn <- 0:200
-#     ll <- seq(1, 5, length.out = 20)
-#     upt <- sapply(nn, dsumuptpois, lambda = ll, m = rep(1000, length(ll)))
-#     noup <- dpois(nn, sum(ll))
-#
-#     expect_true(max(abs(upt - noup)) < .Machine$double.eps^0.25)
-#     expect_true(abs(sum(upt) - sum(noup)) <= .Machine$double.eps^0.75)
-# })
+test_that('when upper truncation is large the `dsumuptpois` function is equivilant to un-truncated', {
+    nn <- 0:200
+    ll <- seq(1, 5, length.out = 20)
+    upt <- sapply(nn, dsumuptpois, lambda = ll, m = rep(10000, length(ll)))
+    noup <- dpois(nn, sum(ll))
 
-
-# ll <- seq(1, 8, length.out = 12)
-# mm <- rep(500, 12)
-# dsumuptpois(40:60, ll, mm)
-#
-# dpois(40:60, sum(ll))
-#
-#
-# ll <- seq(1, 4, length.out = 12)
-# mm <- round(seq(5, 10, length.out = 12))
-# dsumuptpois(20:50, ll, mm)
-
-
-
-# dpois(40:60, sum(ll))
-
+    expect_true(mean(abs(upt - noup)) < 1e-03)
+    expect_true(abs(sum(upt) - sum(noup)) < 1e-03)
+})
 
 
 # ----
 # pmultinom
 
-# test_that('pmultinom output matches published value', {
-#     levin <- 0.8367
-#     hyp <- pmultinom(rep(3, 12), 12, rep(1, 12), 12)
-#     expect_equal(floor(hyp * 1000), floor(levin * 1000))
-# })
+test_that('pmultinom output matches published value', {
+    levin <- 0.8367
+    hyp <- pmultinom(rep(3, 12), 12, rep(1, 12), 12)
+    expect_equal(round(hyp * 1000), round(levin * 1000))
+})
 
 
 # ----
